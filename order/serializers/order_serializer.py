18d5f200cc872ba31db61e7ed173f0cd679e1bee
RNT13 <renatornt13@gmail.com>
2025-10-28 18:51:23 -0300

    Criação do viewset e testes unitarios

--- order/serializers/order_serializer.py
+++ order/serializers/order_serializer.py
@@ -1,11 +1,30 @@
 from rest_framework import serializers
 from order.models import Order
-from product.serializers.product_serializer import ProductSerializer
+from product.models import Product
+from product.serializers import ProductSerializer
 
 
 class OrderSerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True)
+    products = ProductSerializer(many=True, read_only=True)
+    products_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        many=True,
+        write_only=True,
+        source='products'
+    )
+
+    total = serializers.SerializerMethodField()
 
     class Meta:
         model = Order
-        fields = ['id', 'user', 'products', 'total']
+        fields = [
+            'id',
+            'user',
+            'products',
+            'products_id',
+            'total'
+        ]
+        read_only_fields = ['user', 'total']
+
+    def get_total(self, instance):
+        return sum([product.price for product in instance.products.all()])
