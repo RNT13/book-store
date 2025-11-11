2bc8d29ede71e7d6b9a7b9431c84336c2db00cfb
RNT13 <renatornt13@gmail.com>
2025-11-11 08:34:05 -0300

    fix: - Ajustes em product view

--- product/viewsets/product_viewset.py
+++ product/viewsets/product_viewset.py
@@ -3,17 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from product.models import Product
 from product.serializers.product_serializer import ProductSerializer
 
-# from rest_framework.authentication import (
-#     SessionAuthentication,
-#     BasicAuthentication,
-#     TokenAuthentication,
-# )
-# from rest_framework.permissions import IsAuthenticated
-
 
 class ProductViewSet(ModelViewSet):
-    # authentication_classes = [TokenAuthentication]
-    # permission_classes = [IsAuthenticated]
     serializer_class = ProductSerializer
 
-    queryset = Product.objects.all().order_by("id")
+    queryset = Product.objects.all().order_by('id')
